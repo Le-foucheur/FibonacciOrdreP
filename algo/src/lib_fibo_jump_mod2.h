6c2883c5c51396ac60d2c30f7fb5fd131f49d853
louwenus <louwen.fricout@gmail.com>
2024-03-31 15:51:35 +0200

    oups, test uncomented

--- algo/src/lib_fibo_jump_mod2.h
+++ algo/src/lib_fibo_jump_mod2.h
@@ -45,7 +45,7 @@
 
 //autocompletion purpose: comment when building/releasing
 //#define __AVX__
-#define __AVX512F__
+//#define __AVX512F__
 //#define FIBO_AVX512_TEST
 
 #if defined (__AVX512F__) && (!defined (FIBO_NO_AVX512)) && (!defined (FIBO_NO_SSE))
